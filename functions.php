02c6019728c921cbd52a1aee0b560a7672c96c77
gui770 <guilherme770filho@gmail.com>
2018-10-15 20:52:31 -0300

    Commitinicial. Adicionado README.md. Adicionado functions.php

--- functions.php
+++ functions.php
@@ -9,4 +9,10 @@ funtion outraFuncao ($nome){
 echo "ola,{$nome}! ".PHP_EOL;
 
 }
+function outraFuncao($nome){
+echo "ola,{$nome}! Eu sou uma funcao!".PHP_EOL;
+}
+function novaFuncao($msg){
+echo "Sou uma funcao que recebe:{$msg}".PHP_EOL;
+}
 ?>
