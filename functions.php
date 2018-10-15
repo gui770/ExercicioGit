69a03384b6e69a346c5f01b75b351bcd2f5a94e4
gui770 <guilherme770filho@gmail.com>
2018-10-15 20:37:23 -0300

    Commitinicial. Adicionado README.md. Adicionado functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,12 @@
 <? php
 
 function UmaFuncao(){
- echo "Uma funcao que imprime".PHP_EOL;?>
+ echo "Uma funcao que imprime".PHP_EOL;
+
 }
 
+funtion outraFuncao ($nome){
+echo "ola,{$nome}! ".PHP_EOL;
+
+}
+?>
